efcfcd130ac79aa3914d4d3e63e93e6ee45380f5
Laila Yunita <lailayunita01@gmail.com>
2024-08-12 10:00:14 +0700

    refactor: auth

--- apps/web/src/hooks/api/auth/useLogin.tsx
+++ apps/web/src/hooks/api/auth/useLogin.tsx
@@ -3,10 +3,9 @@
 import useAxios from "@/hooks/useAxios";
 import { useAppDispatch } from "@/redux/hooks";
 import { loginAction } from "@/redux/slices/userSlice";
+import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { useRouter } from "next/navigation";
-
-import { useState } from "react";
 import { toast } from "react-toastify";
 
 interface LoginPayload {
@@ -14,32 +13,26 @@ interface LoginPayload {
   password: string;
 }
 
-//kalo bikin custom hook yang di return berupa object
+//pake next auth
 const useLogin = () => {
-  const [isLoading, setIsLoading] = useState(false);
   const { axiosInstance } = useAxios();
-
   const dispatch = useAppDispatch();
   const router = useRouter();
 
-  const login = async (payload: LoginPayload) => {
-    setIsLoading(true);
-    try {
+  return useMutation({
+    mutationFn: async (payload: LoginPayload) => {
       const { data } = await axiosInstance.post("/auth/login", payload);
+      return data;
+    },
+    onSuccess: (data) => {
       dispatch(loginAction(data));
-
-      toast.success("login success");
-
-      router.push("/");
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        toast.error(error.response?.data || "Something wnt wrong!");
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  };
-  return { login, isLoading };
+      toast.success("Login success");
+      router.replace("/");
+    },
+    onError: (error: AxiosError<any>) => {
+      toast.error(error.response?.data);
+    },
+  });
 };
 
 export default useLogin;
